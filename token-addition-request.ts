1138c8630f3c7e29c3bed2df84b945fde0ed5fc9
atmoslp99 <marcjemarc@gmail.com>
2025-12-02 23:34:55 +0100

    Update token-addition-request.ts
    ## Summary
    This pull request adds the FARM LP (FLP) token to the project's requestList for indexing and display.
    
    ## Token Details
    - **Name:** FARM LP
    - **Symbol:** FLP
    - **Decimals:** 8
    - **FA Address:** 0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042
    - **Coin Address:** 0x159790ba3f3ca3b038d9234498ea14390ddcbab13ec96ca17c7396f77da8bf82::FLP::FLP
    - **Logo URL:** https://www.pumpit.pro/token/0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042
    
    ## Code Added
    A TokenRequestType entry was added to `requestList` with the required metadata.

--- token-addition-request.ts
+++ token-addition-request.ts
@@ -1,20 +1,34 @@
-import { TokenRequestType } from "types"
+git add .
+  git commit -m "Add FARM LP (FLP) token to requestList"
+git push origin add-farm-lp-token
 
-const requestList: TokenRequestType[] = []
+## Summary
+This pull request adds the FARM LP (FLP) token to the project's `requestList` for indexing and display.
 
-// Add the details of the token you want to add to the list like this:
-// const tokenRequest: TokenRequestType = {
-//     coinAddress: null,
-//     faAddress: "0x1234567890123456789012345678901234567890",
-//     name: "Token Name",
-//     symbol: "TOKEN",
-//     decimals: 18,
-//     logoUrl: "https://example.com/logo.png",
-//     projectUrl: "https://example.com",
-//     twitterUrl: "https://example.com/twitter",
-//     telegramUrl: "https://example.com/telegram",
-//     discordUrl: "https://example.com/discord",
-//     coinGeckoId: "token-gecko-id",
-//     coinMarketCapId: 1234567890,
-// }
-// and open a PR with the changes
+## Token Details
+- **Name:** FARM LP
+- **Symbol:** FLP
+- **Decimals:** 8
+- **FA Address:** 0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042
+- **Coin Address:** 0x159790ba3f3ca3b038d9234498ea14390ddcbab13ec96ca17c7396f77da8bf82::FLP::FLP
+- **Logo URL:**  
+  https://www.pumpit.pro/token/0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042
+
+## Changes
+A new `TokenRequestType` object was added to `requestList` containing the metadata for FARM LP (FLP).
+
+  const tokenRequest: TokenRequestType = {
+    coinAddress: "0x159790ba3f3ca3b038d9234498ea14390ddcbab13ec96ca17c7396f77da8bf82::FLP::FLP",
+    faAddress: "0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042",
+    name: "FARM LP",
+    symbol: "FLP",
+    decimals: 8,
+    logoUrl: "https://www.pumpit.pro/token/0x7b66011900be87269647b5cce4902a04d3189982ae677a393b2046e55c92042",
+    projectUrl: null,
+    twitterUrl: null,
+    telegramUrl: null,
+    discordUrl: null,
+    coinGeckoId: null,
+    coinMarketCapId: null,
+}
+requestList.push(tokenRequest)
